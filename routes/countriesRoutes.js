4fea46b5246fe06f59abec202e63070aec9fd19b
Marcos <mp2772@gmail.com>
2018-12-08 19:52:35 -0500

    updated routes file to add new api features

--- routes/countriesRoutes.js
+++ routes/countriesRoutes.js
@@ -18,15 +18,52 @@ router.get('/countriesList', (req, res, next) => {
   .then(listOfCountries => {
     // console.log('-------------- ', Object.keys(listOfCountries))
     console.log("========= ", listOfCountries.data);
-    countryPopArray = [];
+    countryPopArray = [['Country', 'Popularity']];
     listOfCountries.data.forEach(oneCountry => {
-      countryPopArray.push([oneCountry.name, oneCountry.population]);
+      // switch(oneCountry.name){
+      //   case "Russian Federation":
+      //   oneCountry.name = "Russia";
+      //   break;
+      //   case "United States of America":
+      //   oneCountry.name = "United States";
+      //   break;
+      //   case "Bolivia (Plurinational State of)":
+      //   oneCountry.name = "Bolivia";
+      //   break;
+      //   case "Venezuela (Bolivarian Republic of)":
+      //   oneCountry.name = "Venezuela";
+      //   break;
+      //   case "United Kingdom of Great Britain and Northern Ireland":
+      //   oneCountry.name = "United Kingdom";
+      //   break;
+      //   case "Iran (Islamic Republic of)":
+      //   oneCountry.name = "Iran";
+      //   break;
+      //   case "Viet Nam":
+      //   oneCountry.name = "Vietnam";
+      //   break;
+      //   case "Congo (Democratic Republic of the)":
+      //   oneCountry.name = "Democratic Republic of the Congo";
+      //   break;
+      //   case "Congo":
+      //   oneCountry.name = "Republic of Congo";
+      //   break;
+      //   case "Tanzania, United Republic of":
+      //   oneCountry.name = "Tanzania";
+      //   break;
+      //   case "Korea (Democratic People\'s Republic of)":
+      //   oneCountry.name = "North Korea";
+      //   break;
+      //   case "Korea (Republic of)":
+      //   oneCountry.name = "South Korea";
+      //   break;
+      // }
+      countryPopArray.push([oneCountry.alpha2Code, oneCountry.population]);
     });
-    data = {
-      countries: listOfCountries.data,
-      countryPopArray: countryPopArray
-    };
-    console.log("=====================>>>>>>>>>>>> ", countryPopArray);
+    console.log("----------------- ", res.locals);
+    res.locals.countryList = JSON.stringify(countryPopArray);
+    console.log(">>>>>>>>>>>>>>>>>> ", res.locals);
+    // console.log("=====================>>>>>>>>>>>> ", countryPopArray);
     res.render('countriesViews/countriesList', {countries: listOfCountries.data});
   })
   .catch(err => {
@@ -37,7 +74,10 @@ router.get('/countriesList', (req, res, next) => {
 router.get('/countryDetails/:name', (req, res, next) => {
   axios.get(`https://restcountries.eu/rest/v2/alpha/${req.params.name}`)
   .then(countryDetails => {
-    console.log("one contry details >>>>>>> ", countryDetails.data.latlng[0]);
+    countryPopArray = [['Country', 'Popularity']];
+    countryPopArray.push([countryDetails.data.alpha2Code, countryDetails.data.population]);
+    res.locals.countryList = JSON.stringify(countryPopArray);
+    // console.log("one contry details >>>>>>> ", countryDetails.data.latlng[0]);
     res.render('countriesViews/countryDetail', {oneCountry: countryDetails.data});
   })
   .catch(err => {
